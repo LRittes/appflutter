51bfd2b7947f89eb5fd38d902eae4a50f665d53b
LRittes <leandro_rittes@hotmail.com>
2021-11-27 13:19:12 -0300

    inter: inclomplete exercise 1-2

--- lib/screens/Home/home.dart
+++ lib/screens/Home/home.dart
@@ -1,11 +1,13 @@
 import 'dart:convert';
-
-import 'package:ativ4/screens/Tela2/tela_2.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+  SharedPreferences? sharedPreferences;
+  Home({Key? key}) : super(key: key) {
+    SharedPreferences.getInstance().then((value) => sharedPreferences = value);
+  }
 
   @override
   _HomeState createState() => _HomeState();
@@ -20,36 +22,54 @@ class _HomeState extends State<Home> {
     return result;
   }
 
+  Image? decode(String? image) {
+    var bytes = base64.decode(image!);
+    Image result = Image.memory(bytes);
+    return result;
+  }
+
   @override
   Widget build(BuildContext context) {
+    Image? image = //widget.sharedPreferences != null
+        // ?
+        decode(
+      widget.sharedPreferences!.getString('img'),
+    );
+    // : null;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Colocando image'),
         centerTitle: true,
       ),
       body: Center(
-        child: ElevatedButton(
-          child: const Text('Tirar foto'),
-          onPressed: () {
-            imagePicker
-                .pickImage(
-                  source: ImageSource.camera,
-                  imageQuality: 25,
-                )
-                .then(
-                  (XFile? value) => {
-                    encode(value!).then((bytes) => {
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (_) {
-                            return Tela2(
-                              bytes: bytes,
-                            );
-                          }))
-                        })
-                  },
-                );
-          },
+        child: Container(
+          margin: const EdgeInsets.all(16),
+          child: image ??
+              const Text(
+                'Insira uma imagem',
+                style: TextStyle(fontSize: 22),
+              ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          imagePicker
+              .pickImage(
+                source: ImageSource.camera,
+                imageQuality: 25,
+              )
+              .then(
+                (XFile? value) => {
+                  encode(value!).then((bytes) => {
+                        setState(() {
+                          widget.sharedPreferences!.setString('img', bytes);
+                        })
+                      })
+                },
+              );
+        },
       ),
     );
   }
